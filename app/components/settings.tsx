aeb620dae0ab204e68331f8338fc0d259b15acd1
MasterH <xksaturn@gmail.com>
2023-05-16 20:53:40 +0800

    添加受控访问

--- app/components/settings.tsx
+++ app/components/settings.tsx
@@ -475,6 +475,25 @@ export function Settings() {
         </List>
 
         <List>
+          
+          {enabledAccessControl ? (
+            <ListItem
+              title={Locale.Settings.AccessCode.Title}
+              subTitle={Locale.Settings.AccessCode.SubTitle}
+            >
+              <PasswordInput
+                value={accessStore.accessCode}
+                type="text"
+                placeholder={Locale.Settings.AccessCode.Placeholder}
+                onChange={(e) => {
+                  accessStore.updateCode(e.currentTarget.value);
+                }}
+              />
+            </ListItem>
+          ) : (
+            <></>
+          )}
+
           <ListItem
             title={Locale.Settings.Prompt.Disable.Title}
             subTitle={Locale.Settings.Prompt.Disable.SubTitle}
