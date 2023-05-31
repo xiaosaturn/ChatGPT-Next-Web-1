5c8ec15d0d4a45c2cbf441b7752d2a073b202256
MasterH <xksaturn@gmail.com>
2023-05-31 14:46:09 +0800

    打印下serverConfig

--- app/components/chat.tsx
+++ app/components/chat.tsx
@@ -506,6 +506,8 @@ export function Chat() {
     const now = new Date();
     const diff = now.getTime() - timestamp.getTime();
     const diffHours = Math.floor(diff / (1000 * 60 * 60));
+    
+    console.log('serverConfig:', serverConfig)
 
     if (diffHours > 24) {
       localStorage.setItem(ipKey, JSON.stringify({
