9ac09cad4c7fb123f59a4a1c54288524d182d341
MasterH <xksaturn@gmail.com>
2023-08-02 13:24:45 +0800

    添加次数验证

--- app/components/login.tsx
+++ app/components/login.tsx
@@ -1,169 +1,210 @@
-
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { AppstoreOutlined, CompassOutlined, SmileOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
-import type { MenuProps } from 'antd';
-import { Input, Button, Select, Space, Menu, Tabs, Spin, message } from 'antd';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import {
+  AppstoreOutlined,
+  CompassOutlined,
+  SmileOutlined,
+  MailOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
+import type { MenuProps } from "antd";
+import { Input, Button, Select, Space, Menu, Tabs, Spin, message } from "antd";
 import { Path, SlotID } from "../constant";
-import { useNodeServerStore } from "@/app/store"
+import { useNodeServerStore } from "@/app/store";
 import { useLocation, useNavigate } from "react-router-dom";
-import styles from './login.module.scss' // 引入自定义的CSS文件
-import { userLogin, userLoginByCode } from '../api/user-info';
+import styles from "./login.module.scss"; // 引入自定义的CSS文件
+import { userLogin, userLoginByCode } from "../api/user-info";
 
 const { Search } = Input;
 
 interface Response {
-    code: number;
-    msg: string;
-    data: any;
-    token: any;
+  code: number;
+  msg: string;
+  data: any;
+  token: any;
 }
 
-
 interface Event {
-    target: EventValue;
+  target: EventValue;
 }
 
 interface EventValue {
-    value: string;
+  value: string;
 }
 
 export function Login() {
-    const navigate = useNavigate();
-    const accessStore = useNodeServerStore();
+  const navigate = useNavigate();
+  const accessStore = useNodeServerStore();
 
-    const [token, setToken] = useState('');
+  const [token, setToken] = useState("");
 
-    const [messageApi, contextHolder] = message.useMessage();
+  const [messageApi, contextHolder] = message.useMessage();
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [verificationCode, setVerificationCode] = useState('');
-    const [tabIndex, setTabIndex] = useState('1');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [verificationCode, setVerificationCode] = useState("");
+  const [tabIndex, setTabIndex] = useState("1");
 
-    const handleEmailChange = (e: Event) => {
-        setEmail(e.target.value);
-    };
+  const handleEmailChange = (e: Event) => {
+    setEmail(e.target.value);
+  };
 
-    const handlePasswordChange = (e: Event) => {
-        setPassword(e.target.value);
-    };
+  const handlePasswordChange = (e: Event) => {
+    setPassword(e.target.value);
+  };
 
-    const handleVerificationCodeChange = (e: Event) => {
-        setVerificationCode(e.target.value);
+  const handleVerificationCodeChange = (e: Event) => {
+    setVerificationCode(e.target.value);
+  };
+
+  const handleToRegister = () => {
+    navigate(Path.Register);
+  };
+
+  const tabChangeEvent = (key: string) => {
+    setTabIndex(key);
+  };
+
+  const getVerificationCode = (e: any) => {
+    // 处理获取验证码逻辑
+    e.preventDefault();
+  };
+
+  const fetchUserLogin = async () => {
+    const res = await userLogin({
+      email,
+      password,
+    });
+    if (res.status == 200) {
+      accessStore.updateToken(res.token);
+      if (typeof window !== "undefined") {
+        // 检查是否在客户端环境下
+        localStorage.setItem("token", res.token);
+      }
+      messageApi.open({
+        type: "success",
+        content: "登录成功",
+      });
+      navigate(Path.UserInfo);
+    } else {
+      messageApi.open({
+        type: "error",
+        content: res.msg ? res.msg : "登录失败，请稍后重试",
+      });
     }
+  };
 
-    const handleToRegister = () => {
-        navigate(Path.Register);
+  const fetchUserLoginCode = async () => {
+    const res = await userLoginByCode({
+      email,
+      verificationCode,
+    });
+    if (res.status == 200) {
+      messageApi.open({
+        type: "success",
+        content: "登录成功",
+      });
+      accessStore.updateToken(res.token);
+      navigate(Path.UserInfo);
+    } else {
+      messageApi.open({
+        type: "error",
+        content: res.msg ? res.msg : "登录失败，请稍后重试",
+      });
     }
-
-    const tabChangeEvent = (key: string) => {
-        setTabIndex(key);
+  };
+  const handleLogin = (e: any) => {
+    // 处理登录逻辑
+    e.preventDefault();
+    if (tabIndex == "1") {
+      // 密码登录
+      fetchUserLogin();
+    } else if (tabIndex == "2") {
+      // 验证码登录
+      messageApi.open({
+        type: "info",
+        content: "暂无开发，敬请期待",
+      });
+      // fetchUserLoginCode()
     }
+  };
 
-    const getVerificationCode = () => {
-        // 处理获取验证码逻辑
-
-    }
-
-    const fetchUserLogin = async () => {
-        const res = await userLogin({
-            email,
-            password
-        });
-        if (res.status == 200) {
-            accessStore.updateToken(res.token);
-            if (typeof window !== "undefined") {
-                // 检查是否在客户端环境下
-                localStorage.setItem("token", res.token);
-            }
-            messageApi.open({
-                type: 'success',
-                content: '登录成功'
-            });
-            navigate(Path.UserInfo);
-        } else {
-            messageApi.open({
-                type: 'error',
-                content: res.msg ? res.msg : '登录失败，请稍后重试'
-            });
-        }
-    }
-
-    const fetchUserLoginCode = async () => {
-        const res = await userLoginByCode({
-            email,
-            verificationCode
-        });
-        if (res.status == 200) {
-            messageApi.open({
-                type: 'success',
-                content: '登录成功'
-            });
-            accessStore.updateToken(res.token);
-            navigate(Path.UserInfo);
-        } else {
-            messageApi.open({
-                type: 'error',
-                content: res.msg ? res.msg : '登录失败，请稍后重试'
-            });
-        }
-    }
-    const handleLogin = (e: any) => {
-        // 处理登录逻辑
-        e.preventDefault();
-        if (tabIndex == '1') {
-            // 密码登录
-            fetchUserLogin();
-        } else if (tabIndex == '2') {
-            // 验证码登录
-            messageApi.open({
-                type: 'info',
-                content: '暂无开发，敬请期待'
-            });
-            // fetchUserLoginCode()
-        }
-    };
-
-    return (
-        <div className={styles["register-container"]}>
-            {contextHolder}
-            <Tabs defaultActiveKey='1' onChange={tabChangeEvent} items={[CompassOutlined, SmileOutlined].map((icon, i) => {
-                return {
-                    label: (
-                        i == 0 ? '密码登录' : '验证码登录'
-                    ),
-                    key: String(i + 1),
-                    children: (
-                        <div>
-                            <h1 className={styles["register-heading"]}>登录</h1>
-                            <form>
-                                <div className={styles["register-input-container"]}>
-                                    <label className={styles["register-label"]}>邮箱:</label>
-                                    <Input className={styles["register-input"]} value={email} onChange={handleEmailChange} placeholder='请输入邮箱' />
-                                </div>
-                                {i == 0 && (
-                                    <div className={styles["register-input-container"]}>
-                                        <label className={styles["register-label"]}>密码:</label>
-                                        <Input.Password className={styles["login-password-input"]} value={password} onChange={handlePasswordChange} placeholder='请输入密码' />
-                                    </div>
-                                )}
-                                {i == 1 && (
-                                    <div className={styles["register-input-container"]} >
-                                        <label className={styles["register-label"]}>验证码:</label>
-                                        <Input className={styles["register-input"]} value={verificationCode} onChange={handleVerificationCodeChange} placeholder='请输入验证码' />
-                                        <button className={styles["register-button"]} onClick={getVerificationCode}>获取验证码</button>
-                                    </div>
-                                )}
-                                <div className={styles["r-container"]}>
-                                    <button type='button' className={styles["register-button"]} style={{ width: '100px' }} onClick={handleLogin}>登录</button>
-                                    <div className={styles["to-register"]} style={{ width: '200px' }} onClick={handleToRegister}>没有账号？去注册</div>
-                                </div>
-                            </form>
-                        </div>
-                    )
-                }
-            })} />
-        </div>
-    );
+  return (
+    <div className={styles["register-container"]}>
+      {contextHolder}
+      <Tabs
+        defaultActiveKey="1"
+        onChange={tabChangeEvent}
+        items={[CompassOutlined, SmileOutlined].map((icon, i) => {
+          return {
+            label: i == 0 ? "密码登录" : "验证码登录",
+            key: String(i + 1),
+            children: (
+              <div>
+                <h1 className={styles["register-heading"]}>登录</h1>
+                <form>
+                  <div className={styles["register-input-container"]}>
+                    <label className={styles["register-label"]}>邮箱:</label>
+                    <Input
+                      className={styles["register-input"]}
+                      value={email}
+                      onChange={handleEmailChange}
+                      placeholder="请输入邮箱"
+                    />
+                  </div>
+                  {i == 0 && (
+                    <div className={styles["register-input-container"]}>
+                      <label className={styles["register-label"]}>密码:</label>
+                      <Input.Password
+                        className={styles["login-password-input"]}
+                        value={password}
+                        onChange={handlePasswordChange}
+                        placeholder="请输入密码"
+                      />
+                    </div>
+                  )}
+                  {i == 1 && (
+                    <div className={styles["register-input-container"]}>
+                      <label className={styles["register-label"]}>
+                        验证码:
+                      </label>
+                      <Input
+                        className={styles["register-input"]}
+                        value={verificationCode}
+                        onChange={handleVerificationCodeChange}
+                        placeholder="请输入验证码"
+                      />
+                      <button
+                        className={styles["register-button"]}
+                        onClick={getVerificationCode}
+                      >
+                        获取验证码
+                      </button>
+                    </div>
+                  )}
+                  <div className={styles["r-container"]}>
+                    <button
+                      type="button"
+                      className={styles["register-button"]}
+                      style={{ width: "100px" }}
+                      onClick={handleLogin}
+                    >
+                      登录
+                    </button>
+                    <div
+                      className={styles["to-register"]}
+                      style={{ width: "200px" }}
+                      onClick={handleToRegister}
+                    >
+                      没有账号？去注册
+                    </div>
+                  </div>
+                </form>
+              </div>
+            ),
+          };
+        })}
+      />
+    </div>
+  );
 }
--- app/components/register.tsx
+++ app/components/register.tsx
@@ -1,191 +1,230 @@
-
-import React, { useEffect, useState } from 'react';
-import { Input, Button, Select, Space, message } from 'antd';
-import styles from './register.module.scss' // 引入自定义的CSS文件
-import { userRegister, getVerificationCode } from '../api/user-info';
+import React, { useEffect, useState } from "react";
+import { Input, Button, Select, Space, message } from "antd";
+import styles from "./register.module.scss"; // 引入自定义的CSS文件
+import { userRegister, getVerificationCode } from "../api/user-info";
 import { useNavigate } from "react-router-dom";
 import { Path, SlotID } from "../constant";
-import { useNodeServerStore } from '@/app/store';
+import { useNodeServerStore } from "@/app/store";
 
 const { Search } = Input;
 
 interface Event {
-    target: EventValue;
+  target: EventValue;
 }
 
 interface EventValue {
-    value: string;
+  value: string;
 }
 
 export function Register() {
-    const accessStore = useNodeServerStore.getState();
+  const accessStore = useNodeServerStore.getState();
 
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
-    const [verificationCode, setVerificationCode] = useState('');
-    const [countTime, setCountTime] = useState(0);
-    const [countTimeStr, setCountTimeStr] = useState('获取验证码');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [verificationCode, setVerificationCode] = useState("");
+  const [countTime, setCountTime] = useState(0);
+  const [countTimeStr, setCountTimeStr] = useState("获取验证码");
 
-    const [messageApi, contextHolder] = message.useMessage();
+  const [messageApi, contextHolder] = message.useMessage();
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        if (countTime == 0) {
-            setCountTimeStr(preStr => '获取验证码');
-            return;
-        }
-
-        const timer = setInterval(() => {
-            setCountTime(prevCount => prevCount - 1);
-            setCountTimeStr(preStr => countTime + 's后重新获取');
-        }, 1000);
-        
-        return () => clearInterval(timer);
-    }, [countTime]);
-
-    const handleNameChange = (e: Event) => {
-        setName(e.target.value);
-    };
-
-    const handleEmailChange = (e: Event) => {
-        setEmail(e.target.value);
-    };
-
-    const handlePasswordChange = (e: Event) => {
-        setPassword(e.target.value);
-    };
-
-    const handleConfirmPasswordChange = (e: Event) => {
-        setConfirmPassword(e.target.value);
+  useEffect(() => {
+    if (countTime == 0) {
+      setCountTimeStr((preStr) => "获取验证码");
+      return;
     }
 
-    const handleVerificationCodeChange = (e: Event) => {
-        setVerificationCode(e.target.value);
+    const timer = setInterval(() => {
+      setCountTime((prevCount) => prevCount - 1);
+      setCountTimeStr((preStr) => countTime + "s后重新获取");
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, [countTime]);
+
+  const handleNameChange = (e: Event) => {
+    setName(e.target.value);
+  };
+
+  const handleEmailChange = (e: Event) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e: Event) => {
+    setPassword(e.target.value);
+  };
+
+  const handleConfirmPasswordChange = (e: Event) => {
+    setConfirmPassword(e.target.value);
+  };
+
+  const handleVerificationCodeChange = (e: Event) => {
+    setVerificationCode(e.target.value);
+  };
+
+  const getVeriCode = (e: any) => {
+    // 处理获取验证码逻辑
+    e.preventDefault();
+    getCode();
+  };
+
+  const getCode = async () => {
+    // 处理获取验证码逻辑
+    if (!validateEmail(email)) {
+      messageApi.open({
+        type: "error",
+        content: "邮箱格式不正确",
+      });
+      return;
     }
-
-    const getVeriCode = (e: any) => {
-        // 处理获取验证码逻辑
-        e.preventDefault();
-        getCode();
+    const res = await getVerificationCode({ email });
+    if (res.status == 200) {
+      setCountTime(60);
+      messageApi.open({
+        type: "success",
+        content: "验证码已发送",
+      });
+    } else {
+      messageApi.open({
+        type: "error",
+        content: res.msg ? res.msg : "验证码发送失败，请稍后重试",
+      });
     }
+  };
 
-    const getCode = async () => {
-        // 处理获取验证码逻辑
-        if (!validateEmail(email)) {
-            messageApi.open({
-                type: 'error',
-                content: '邮箱格式不正确'
-            });
-            return;
-        }
-        const res = await getVerificationCode({ email });
-        if (res.status == 200) {
-            setCountTime(60);
-            messageApi.open({
-                type: 'success',
-                content: '验证码已发送'
-            });
-        } else {
-            messageApi.open({
-                type: 'error',
-                content: res.msg ? res.msg : '验证码发送失败，请稍后重试'
-            });
-        }
+  const handleRegister = (e: any) => {
+    e.preventDefault();
+    // 处理注册逻辑
+    if (!validateEmail(email)) {
+      messageApi.open({
+        type: "error",
+        content: "邮箱格式不正确",
+      });
+      return;
     }
-
-    const handleRegister = (e: any) => {
-        e.preventDefault();
-        // 处理注册逻辑
-        if (!validateEmail(email)) {
-            messageApi.open({
-                type: 'error',
-                content: '邮箱格式不正确'
-            });
-            return;
-        }
-        if (password !== confirmPassword) {
-            messageApi.open({
-                type: 'error',
-                content: '两次密码不一致'
-            });
-            return;
-        }
-        if (!checkPassowrd(password)) {
-            messageApi.open({
-                type: 'error',
-                content: '密码格式不正确，至少包含一个数字，一个大写字母，一个小写字母，一个特殊符号，8~24位'
-            });
-            return;
-        }
-        if (!verificationCode || verificationCode == '' || verificationCode == undefined || verificationCode == null) {
-            messageApi.open({
-                type: 'error',
-                content: '验证码格式不正确'
-            });
-            return;
-        }
-        toRegister();
-    };
-
-    const toRegister = async () => {
-        const res = await userRegister({ email, password, verificationCode });
-        if (res.status == 200) {
-            if (typeof window !== "undefined") {
-                // 检查是否在客户端环境下
-                localStorage.setItem("token", res.token);
-            }
-            messageApi.open({
-                type: 'success',
-                content: '注册成功'
-            });
-            accessStore.updateToken(res.token);
-            navigate(Path.UserInfo);
-        } else {
-            messageApi.open({
-                type: 'error',
-                content: res.msg ? res.msg : '注册失败'
-            });
-        }
+    if (password !== confirmPassword) {
+      messageApi.open({
+        type: "error",
+        content: "两次密码不一致",
+      });
+      return;
     }
+    if (!checkPassowrd(password)) {
+      messageApi.open({
+        type: "error",
+        content:
+          "密码格式不正确，至少包含一个数字，一个大写字母，一个小写字母，一个特殊符号，8~24位",
+      });
+      return;
+    }
+    if (
+      !verificationCode ||
+      verificationCode == "" ||
+      verificationCode == undefined ||
+      verificationCode == null
+    ) {
+      messageApi.open({
+        type: "error",
+        content: "验证码格式不正确",
+      });
+      return;
+    }
+    toRegister();
+  };
 
-    return (
-        <div className={styles["register-container"]}>
-            {contextHolder}
-            <div>
-                <h1 className={styles["register-heading"]}>注册</h1>
-                <form>
-                    <div className={styles["register-input-container"]}>
-                        <label className={styles["register-label"]}>邮箱:</label>
-                        <Input className={styles["register-input"]} value={email} onChange={handleEmailChange} placeholder='请输入邮箱' />
-                    </div>
-                    <div className={styles["register-input-container"]}>
-                        <label className={styles["register-label"]}>密码:</label>
-                        <Input.Password className={styles["register-password-input"]}  value={password} onChange={handlePasswordChange} 
-                        placeholder='请输入密码' />
-                    </div>
-                    <div className={styles["register-input-container"]}>
-                        <label className={styles["register-label"]}>确认密码:</label>
-                        <Input.Password className={styles["register-password-input"]} value={confirmPassword} onChange={handleConfirmPasswordChange} placeholder='再次输入密码' />
-                    </div>
-                    <div className={styles["register-input-container"]}>
-                        <label className={styles["register-label"]}>验证码:</label>
-                        <Input className={styles["register-input"]} value={verificationCode} onChange={handleVerificationCodeChange} placeholder='请输入验证码' />
-                        {
-                            countTime > 0 ? (
-                                <button className={styles["register-button"]} disabled>{countTimeStr}</button>
-                            ) : (
-                                <button className={styles["register-button"]} onClick={getVeriCode}>获取验证码</button>
-                            )
-                        }
-                    </div>
-                    <button className={styles["register-button"]} style={{ width: '100px', marginLeft: '120px' }} onClick={handleRegister}>注册</button>
-                </form>
-            </div>
-        </div>
-    );
+  const toRegister = async () => {
+    const res = await userRegister({ email, password, verificationCode });
+    if (res.status == 200) {
+      if (typeof window !== "undefined") {
+        // 检查是否在客户端环境下
+        localStorage.setItem("token", res.token);
+      }
+      messageApi.open({
+        type: "success",
+        content: "注册成功",
+      });
+      if (typeof window !== "undefined") {
+        // 检查是否在客户端环境下
+        localStorage.setItem("token", res.token);
+      }
+      accessStore.updateToken(res.token);
+      navigate(Path.UserInfo);
+    } else {
+      messageApi.open({
+        type: "error",
+        content: res.msg ? res.msg : "注册失败",
+      });
+    }
+  };
+
+  return (
+    <div className={styles["register-container"]}>
+      {contextHolder}
+      <div>
+        <h1 className={styles["register-heading"]}>注册</h1>
+        <form>
+          <div className={styles["register-input-container"]}>
+            <label className={styles["register-label"]}>邮箱:</label>
+            <Input
+              className={styles["register-input"]}
+              value={email}
+              onChange={handleEmailChange}
+              placeholder="请输入邮箱"
+            />
+          </div>
+          <div className={styles["register-input-container"]}>
+            <label className={styles["register-label"]}>密码:</label>
+            <Input.Password
+              className={styles["register-password-input"]}
+              value={password}
+              onChange={handlePasswordChange}
+              placeholder="请输入密码"
+            />
+          </div>
+          <div className={styles["register-input-container"]}>
+            <label className={styles["register-label"]}>确认密码:</label>
+            <Input.Password
+              className={styles["register-password-input"]}
+              value={confirmPassword}
+              onChange={handleConfirmPasswordChange}
+              placeholder="再次输入密码"
+            />
+          </div>
+          <div className={styles["register-input-container"]}>
+            <label className={styles["register-label"]}>验证码:</label>
+            <Input
+              className={styles["register-input"]}
+              value={verificationCode}
+              onChange={handleVerificationCodeChange}
+              placeholder="请输入验证码"
+            />
+            {countTime > 0 ? (
+              <button className={styles["register-button"]} disabled>
+                {countTimeStr}
+              </button>
+            ) : (
+              <button
+                className={styles["register-button"]}
+                onClick={getVeriCode}
+              >
+                获取验证码
+              </button>
+            )}
+          </div>
+          <button
+            className={styles["register-button"]}
+            style={{ width: "100px", marginLeft: "120px" }}
+            onClick={handleRegister}
+          >
+            注册
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 /**
@@ -195,12 +234,12 @@ export function Register() {
  * @returns {boolean} - Returns true if the email format is valid, otherwise false.
  */
 function validateEmail(email: string) {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-  }
-  
-const checkPassowrd = (value: string) => {
-    const reg = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]).{8,24}$/;
-    return reg.test(value);
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
 }
 
+const checkPassowrd = (value: string) => {
+  const reg =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]).{8,24}$/;
+  return reg.test(value);
+};
